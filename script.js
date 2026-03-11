bb66467b416ebbbefd66cf0b80e1d077879df5f5
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-03-11 19:18:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ detail.style.display = "none";
 }
 
 }
+
+
+/* 順位グラフ */
+
+const ctx = document.getElementById('rankChart');
+
+new Chart(ctx, {
+type: 'line',
+data: {
+labels: ['2017','2018','2019','2020','2021','2022','2023'],
+datasets: [{
+label: '中日ドラゴンズ順位',
+data: [5,5,6,3,5,6,6],
+borderWidth: 3
+}]
+},
+options: {
+scales: {
+y: {
+reverse: true,
+min: 1,
+max: 6
+}
+}
+}
+});
