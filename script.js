fd04f57d4d94b0b8561ae38df9d5ab64709a2686
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-03-11 18:47:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Dragons Fan Site Loaded");
+function showOchiai(){
+
+var detail = document.getElementById("ochiai-detail");
+
+if(detail.style.display === "none"){
+detail.style.display = "block";
+}else{
+detail.style.display = "none";
+}
+
+}
