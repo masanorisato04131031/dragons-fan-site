4059db7c051e58c761a1c70b82b5a10338ffe861
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-03-12 11:51:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Dragons Fan Site Loaded");
+const images = [
+
+"https://picsum.photos/1200/400?1",
+"https://picsum.photos/1200/400?2",
+"https://picsum.photos/1200/400?3"
+
+]
+
+let current = 0
+
+const slide = document.getElementById("slide")
+
+function changeSlide(){
+
+slide.src = images[current]
+
+current++
+
+if(current >= images.length){
+
+current = 0
+
+}
+
+}
+
+changeSlide()
+
+setInterval(changeSlide,3000)
