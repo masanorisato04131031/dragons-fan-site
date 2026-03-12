1ae62a0591e57b3338e0932e24020eeb97b91e7b
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-03-12 12:26:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const images=[
-
-"./stadium.jpg.jpg
-
-]
-
-let current=0
-
-const slide=document.getElementById("slide")
-
-function changeSlide(){
-
-slide.src=images[current]
-
-}
-
-changeSlide()
-
+console.log("Dragons Fan Site Loaded");
