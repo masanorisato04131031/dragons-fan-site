aa18c8fa2a967a0045c2c4752058ee80c6af8a32
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-03-12 14:01:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-
 const ctx = document.getElementById('statsChart');
 
 new Chart(ctx, {
 
-type: 'line',
+type:'line',
 
-data: {
+data:{
 
-labels: ['4月','5月','6月','7月','8月'],
+labels:['4月','5月','6月','7月','8月'],
 
-datasets: [{
+datasets:[{
 
-label: 'チーム勝利数',
+label:'チーム勝利数',
 
-data: [10,18,25,35,42],
+data:[10,18,25,35,42],
 
 borderWidth:3
 
@@ -22,21 +21,21 @@ borderWidth:3
 },
 
 options:{
-
 responsive:true
-
 }
 
 });
 
+
+
 function openTakahashi(){
 
-document.getElementById("takahashiModal").style.display = "block";
+document.getElementById("takahashiModal").style.display="block";
 
 }
 
 function closeTakahashi(){
 
-document.getElementById("takahashiModal").style.display = "none";
+document.getElementById("takahashiModal").style.display="none";
 
 }
